99e2469068da8da24bd48721cab19b1928c64d90
Lance Art P Fortaleza <lpfortaleza@student.apc.edu.ph>
2026-02-16 15:04:33 +0800

    .

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Guestbook from './Guestbook'; // Imports your Guestbook component
+import Guestbook from './guestbook'; // Imports your Guestbook component
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
